e86a1c26333b25c99bcd0144e77eaff3b72796b3
Kirill <strelOK042021@gmail.com>
2022-02-17 00:03:43 +0200

    Workable version

---
+++ locations_app.py
@@ -0,0 +1,68 @@
+import tweepy
+
+from flask import Flask, render_template, Markup, flash
+from flask import request
+import folium
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+from geopy.geocoders import Nominatim
+
+consumer_key = "enter_yourself"
+consumer_secret = "enter_yourself"
+access_token_key = "enter_yourself"
+access_token_secret = "enter_yourself"
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'enter_yourself'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    folium_map = folium.Map(zoom_start=1)
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+
+        locations_data = get_users_locations(username)
+
+        for user in locations_data:
+            if user[2]:
+                folium.Marker(
+                    [user[2].latitude, user[2].longitude],
+                    popup="<p><b>" + user[0] + "</b><br>" + user[1] + "</p>",
+                ).add_to(folium_map)
+
+    map_html = Markup(folium_map._repr_html_())
+    flash(map_html)
+
+    return render_template('index.html')
+
+
+def get_users_locations(user_name: str):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token_key, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    geolocator = Nominatim(user_agent="geomap-app")
+
+    users_data = []
+
+    for follower in api.friends(screen_name=user_name):
+        users_location = geolocator.geocode(follower.location)
+
+        # users_location = (0, 0)
+        users_data.append(
+            (follower.screen_name, follower.location, users_location))
+
+        print(follower.screen_name)
+
+    return users_data
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
